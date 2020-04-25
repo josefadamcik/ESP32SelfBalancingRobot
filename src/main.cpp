66953bc3b56f725d6940b0489f7890626b133c5a
Josef Adamcik <josef.adamcik@gmail.com>
2020-04-25 19:17:35 +0200

    OTA workign like a charm

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include "Arduino.h"
+#include "ArduinoOTA.h"
+#include "keys.h"
 // #define CUSTOM_SETTINGS
 // #define INCLUDE_TERMINAL_MODULE
 // #define INCLUDE_MOTORCONTROL_MODULE
@@ -69,68 +71,69 @@ void dmpDataReady() {
 // ================================================================
 
 void setup() {
-  // digitalWrite(MOTOR_A1, LOW);
-  // digitalWrite(MOTOR_A2, LOW);
-  // digitalWrite(MOTOR_B1, LOW);
-  // digitalWrite(MOTOR_B2, LOW);
-  // ledcSetup(pwmMotorAChannel, pwmFreq, pwmResolution);
-  // ledcSetup(pwmMotorBChannel, pwmFreq, pwmResolution);
-  MotorControl.attachMotors(MOTOR_A1, MOTOR_A2, MOTOR_B1, MOTOR_B2);
   Serial.begin(115200);
-}
+  Serial.println("Connecting to wifi");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(MYSSID, MYPASS);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+  ArduinoOTA.setHostname("BalancingBotESP32");
+  ArduinoOTA
+    .onStart([]() {
+      MotorControl.motorsStop();
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
 
-int counter = 0;
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+
+  Serial.println("Ready tralal");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.println("Setup motors");
+  MotorControl.attachMotors(MOTOR_A1, MOTOR_A2, MOTOR_B1, MOTOR_B2);
+}
 
 // ================================================================
 // ===                    MAIN PROGRAM LOOP                     ===
 // ================================================================
 
 void loop() {
+    ArduinoOTA.handle();  
   Serial.println("loop");
   for (int i = 0; i <= 100; i++) {
     MotorControl.motorForward(0, i);
     MotorControl.motorForward(1, i);
     delay(100);
   }
-  // ledcAttachPin(MOTOR_A1, pwmMotorAChannel);
-  // ledcAttachPin(MOTOR_B1, pwmMotorBChannel);
-  // digitalWrite(MOTOR_A2, LOW); //Fast decay
-  // digitalWrite(MOTOR_B2, LOW);
-  // for (int i = pwmMinDuty; i <= pwmMaxDuty; i++) {
-  //   Serial.println(i);
-  //   ledcWrite(pwmMotorAChannel, i);
-  //   ledcWrite(pwmMotorBChannel, i);
-  //   delay(10);
-  // }
-  // delay(1000);
-  // for (int i = pwmMaxDuty; i >= pwmMinDuty; i--) {
-  //   Serial.println(i);
-  //   ledcWrite(pwmMotorAChannel, i);
-  //   ledcWrite(pwmMotorBChannel, i);
-  //   delay(10);
-  // }
-  // ledcDetachPin(MOTOR_A1);
-  // ledcDetachPin(MOTOR_B1);
-  // ledcAttachPin(MOTOR_A2, pwmMotorAChannel);
-  // ledcAttachPin(MOTOR_B2, pwmMotorBChannel);
-  // digitalWrite(MOTOR_A1, LOW); //Fast decay
-  // digitalWrite(MOTOR_B1, LOW);
-  // delay(100);
-  // for (int i = pwmMinDuty; i <= pwmMaxDuty; i++) {
-  //   Serial.println(i);
-  //   ledcWrite(pwmMotorAChannel, i);
-  //   ledcWrite(pwmMotorBChannel, i);
-  //   delay(10);
-  // }
-  // delay(1000);
-  // for (int i = pwmMaxDuty; i >= pwmMinDuty; i--) {
-  //   Serial.println(i);
-  //   ledcWrite(pwmMotorAChannel, i);
-  //   ledcWrite(pwmMotorBChannel, i);
-  //   delay(10);
-  // }
-  // delay(100);
-  // ledcDetachPin(MOTOR_A2);
-  // ledcDetachPin(MOTOR_B2);
-
+  delay(1000);
+  for (int i = 100; i >= 0; i--) {
+    MotorControl.motorForward(0, i);
+    MotorControl.motorForward(1, i);
+    delay(100);
+  }
 }
