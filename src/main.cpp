8dea70d7578eee1106f0f3fdbc75a16be5ef9f42
Josef Adamcik <josef.adamcik@gmail.com>
2020-04-25 21:44:02 +0200

    Trying to make mpu6050 work again

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include "Arduino.h"
 #include "ArduinoOTA.h"
 #include "keys.h"
-// #define CUSTOM_SETTINGS
+// #define CUSTOM_SETTINGS? 
 // #define INCLUDE_TERMINAL_MODULE
 // #define INCLUDE_MOTORCONTROL_MODULE
 // #include <DabbleESP32.h>
-// #include "I2Cdev.h"
-// #include "MPU6050_6Axis_MotionApps_V6_12.h"
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps_V6_12.h"
 #include "ESP32MotorControl.h"
 
 // Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
@@ -15,8 +15,7 @@
 #include "Wire.h"
 #endif
 
-// MPU6050 mpu;
-// #define OUTPUT_TEAPOT
+MPU6050 mpu;
 #define INTERRUPT_PIN 19  // use pin 2 on Arduino Uno & most boards
 #define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
 bool blinkState = false;
@@ -35,22 +34,22 @@ const int pwmMaxDuty = 1023;
 ESP32MotorControl motorControl = ESP32MotorControl();
 
 // // MPU control/status vars
-// bool dmpReady = false;  // set true if DMP init was successful
-// uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
-// uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
-// uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
-// uint16_t fifoCount;     // count of all bytes currently in FIFO
-// uint8_t fifoBuffer[64]; // FIFO storage buffer
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
 
-// // orientation/motion vars
-// Quaternion q;           // [w, x, y, z]         quaternion container
-// VectorInt16 aa;         // [x, y, z]            accel sensor measurements
-// VectorInt16 gy;         // [x, y, z]            gyro sensor measurements
-// VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-// VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
-// VectorFloat gravity;    // [x, y, z]            gravity vector
-// float euler[3];         // [psi, theta, phi]    Euler angle container
-// float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 gy;         // [x, y, z]            gyro sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
 // packet structure for InvenSense teapot demo
 // uint8_t teapotPacket[14] = { '$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x00, '\r', '\n' };
@@ -72,6 +71,69 @@ void dmpDataReady() {
 
 void setup() {
   Serial.begin(115200);
+
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    Wire.begin();
+    Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+  #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+    Fastwire::setup(400, true);
+  #endif
+
+  //MPU6050
+  Serial.println(F("Initializing MPU6050..."));
+  mpu.initialize();
+  pinMode(INTERRUPT_PIN, INPUT);
+  Serial.println(F("Testing device connections..."));
+  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+  // supply your own gyro offsets here, scaled for min sensitivity
+  //           X Accel  Y Accel  Z Accel   X Gyro   Y Gyro   Z Gyro
+//OFFSETS    -3498,    -846,    1662,     155,      17,       6
+//           X Accel  Y Accel  Z Accel   X Gyro   Y Gyro   Z Gyro
+//OFFSETS    -3500,    -868,    1660,     142,      18,       5
+//           X Accel  Y Accel  Z Accel   X Gyro   Y Gyro   Z Gyro
+//OFFSETS    -3500,    -868,    1660,     142,      18,       5
+  mpu.setXGyroOffset(51);
+  mpu.setYGyroOffset(8);
+  mpu.setZGyroOffset(21);
+  mpu.setXAccelOffset(1150);
+  mpu.setYAccelOffset(-50);
+  mpu.setZAccelOffset(1060);
+  // make sure it worked (returns 0 if so)
+  if (devStatus == 0) {
+    // Calibration Time: generate offsets and calibrate our MPU6050
+    mpu.CalibrateAccel(6);
+    mpu.CalibrateGyro(6);
+    Serial.println();
+    mpu.PrintActiveOffsets();
+    // turn on the DMP, now that it's ready
+    Serial.println(F("Enabling DMP..."));
+    mpu.setDMPEnabled(true);
+
+    // enable Arduino interrupt detection
+    Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+    Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+    Serial.println(F(")..."));
+    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+
+    // set our DMP Ready flag so the main loop() function knows it's okay to use it
+    Serial.println(F("DMP ready! Waiting for first interrupt..."));
+    dmpReady = true;
+
+    // get expected DMP packet size for later comparison
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  } else {
+    // ERROR!
+    // 1 = initial memory load failed
+    // 2 = DMP configuration updates failed
+    // (if it's going to break, usually the code will be 1)
+    Serial.print(F("DMP Initialization failed (code "));
+    Serial.print(devStatus);
+    Serial.println(F(")"));
+  }
+
+  //WIFI
   Serial.println("Connecting to wifi");
   WiFi.mode(WIFI_STA);
   WiFi.begin(MYSSID, MYPASS);
@@ -80,6 +142,7 @@ void setup() {
     delay(5000);
     ESP.restart();
   }
+  //OTA
   ArduinoOTA.setHostname("BalancingBotESP32");
   ArduinoOTA
     .onStart([]() {
@@ -120,9 +183,9 @@ void setup() {
       ArduinoOTA.handle();
       delay(500);
   }
-  Serial.println("Continue.");
-  Serial.println("Setup motors");
-  motorControl.attachMotors(MOTOR_A1, MOTOR_A2, MOTOR_B1, MOTOR_B2);
+  Serial.println("");
+  // Serial.println("Setup motors");
+  // motorControl.attachMotors(MOTOR_A1, MOTOR_A2, MOTOR_B1, MOTOR_B2);
 }
 
 // ================================================================
@@ -130,17 +193,48 @@ void setup() {
 // ================================================================
 
 void loop() {
-  ArduinoOTA.handle();
   Serial.println("loop");
-  for (int i = 0; i <= 100; i++) {
-    motorControl.motorForward(0, i);
-    motorControl.motorForward(1, i);
-    delay(100);
+  Serial.println(dmpReady);
+  if (!dmpReady) return;
+  // if programming failed, don't try to do anything
+  if (mpuInterrupt) {
+    Serial.println("Interrupt!");
+    mpuInterrupt = false;
   }
-  delay(1000);
-  for (int i = 100; i >= 0; i--) {
-    motorControl.motorForward(0, i);
-    motorControl.motorForward(1, i);
-    delay(100);
+
+  // read a packet from FIFO
+  if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet 
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+    Serial.print("ypr\t");
+    Serial.print(ypr[0] * 180 / M_PI);
+    Serial.print("\t");
+    Serial.print(ypr[1] * 180 / M_PI);
+    Serial.print("\t");
+    Serial.print(ypr[2] * 180 / M_PI);
+
+    // display Euler angles in degrees
+    mpu.dmpGetEuler(euler, &q);
+    Serial.print("euler\t");
+    Serial.print(euler[0] * 180 / M_PI);
+    Serial.print("\t");
+    Serial.print(euler[1] * 180 / M_PI);
+    Serial.print("\t");
+    Serial.println(euler[2] * 180 / M_PI);
   }
+
+  // ArduinoOTA.handle();
+  // Serial.println("loop");
+  // for (int i = 0; i <= 100; i++) {
+  //   motorControl.motorForward(0, i);
+  //   motorControl.motorForward(1, i);
+  //   delay(100);
+  // }
+  // delay(1000);
+  // for (int i = 100; i >= 0; i--) {
+  //   motorControl.motorForward(0, i);
+  //   motorControl.motorForward(1, i);
+  //   delay(100);
+  // }
 }
