2dcdebc08bcdceaec63d17c1f2718d891a9efe41
Josef Adamcik <josef.adamcik@gmail.com>
2020-04-26 09:34:59 +0200

    Angle controlls motors speed. OTA

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,31 @@
-#include "Arduino.h"
-#include "ArduinoOTA.h"
-#include "keys.h"
-// #define CUSTOM_SETTINGS? 
+
+// #include "Arduino.h"s
+// #define CUSTOM_SETTINGS
 // #define INCLUDE_TERMINAL_MODULE
 // #define INCLUDE_MOTORCONTROL_MODULE
 // #include <DabbleESP32.h>
-#include "I2Cdev.h"
-#include "math.h"
-#include "MPU6050_6Axis_MotionApps_V6_12.h"
-#include "ESP32MotorControl.h"
 
-// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
-// is used in I2Cdev.h
+// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
+// for both classes must be in the include path of your project
+#include "I2Cdev.h"
+
+#include "MPU6050_6Axis_MotionApps_V6_12.h"
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
 #include "Wire.h"
 #endif
 
-MPU6050 mpu;
-#define INTERRUPT_PIN 19 
-bool blinkState = false;
+#include "Arduino.h"
+#include "ArduinoOTA.h"
+#include "keys.h"
+#include "math.h"
+#include "ESP32MotorControl.h" 
 
-#define MOTOR_A1 32
-#define MOTOR_A2 33
-#define MOTOR_B1 25
-#define MOTOR_B2 26
+MPU6050 mpu; 
+// #define OUTPUT_READABLE_EULER
+#define OUTPUT_READABLE_YAWPITCHROLL
+#define INTERRUPT_PIN 19  // use pin 2 on Arduino Uno & most boards
 
-const int pwmFreq = 10000;
-const int pwmMotorAChannel = 0;
-const int pwmMotorBChannel = 1;
-const int pwmResolution = 10;
-const int pwmMinDuty = 400;
-const int pwmMaxDuty = 1023;
-ESP32MotorControl motorControl = ESP32MotorControl();
-
-// // MPU control/status vars
+// MPU control/status vars
 bool dmpReady = false;  // set true if DMP init was successful
 uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
 uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
@@ -52,53 +44,53 @@ float euler[3];         // [psi, theta, phi]    Euler angle container
 float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
 // packet structure for InvenSense teapot demo
-// uint8_t teapotPacket[14] = { '$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x00, '\r', '\n' };
+uint8_t teapotPacket[14] = { '$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x00, '\r', '\n' };
 
-
-
-// ================================================================
-// ===               INTERRUPT DETECTION ROUTINE                ===
-// ================================================================
-
-volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
-void dmpDataReady() {
+static volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void IRAM_ATTR dmpDataReady() {
   mpuInterrupt = true;
 }
+
+#define MOTOR_A1 32
+#define MOTOR_A2 33
+#define MOTOR_B1 26
+#define MOTOR_B2 25
+ESP32MotorControl motorControl = ESP32MotorControl();
+
 
 // ================================================================
 // ===                      INITIAL SETUP                       ===
 // ================================================================
 
 void setup() {
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+  Wire.begin();
+  Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+  Fastwire::setup(400, true);
+#endif
+
   Serial.begin(115200);
 
-  // join I2C bus (I2Cdev library doesn't do this automatically)
-  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-    Wire.begin();
-    Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
-  #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-    Fastwire::setup(400, true);
-  #endif
-
-  //MPU6050
-  Serial.println(F("Initializing MPU6050..."));
+  // initialize device
+  Serial.println(F("Initializing I2C devices..."));
   mpu.initialize();
   pinMode(INTERRUPT_PIN, INPUT);
+
+  // verify connection
   Serial.println(F("Testing device connections..."));
   Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
-  // supply your own gyro offsets here, scaled for min sensitivity
-  //           X Accel  Y Accel  Z Accel   X Gyro   Y Gyro   Z Gyro
-//OFFSETS    -3498,    -846,    1662,     155,      17,       6
-//           X Accel  Y Accel  Z Accel   X Gyro   Y Gyro   Z Gyro
-//OFFSETS    -3500,    -868,    1660,     142,      18,       5
-//           X Accel  Y Accel  Z Accel   X Gyro   Y Gyro   Z Gyro
-//OFFSETS    -3500,    -868,    1660,     142,      18,       5
-  mpu.setXGyroOffset(51);
-  mpu.setYGyroOffset(8);
-  mpu.setZGyroOffset(21);
-  mpu.setXAccelOffset(1150);
-  mpu.setYAccelOffset(-50);
-  mpu.setZAccelOffset(1060);
+
+  // load and configure the DMP
+  Serial.println(F("Initializing DMP..."));
+  devStatus = mpu.dmpInitialize();
+  mpu.setXGyroOffset(142);
+  mpu.setYGyroOffset(18);
+  mpu.setZGyroOffset(5);
+  mpu.setXAccelOffset(-3500);
+  mpu.setYAccelOffset(-868);
+  mpu.setZAccelOffset(1660);
   // make sure it worked (returns 0 if so)
   if (devStatus == 0) {
     // Calibration Time: generate offsets and calibrate our MPU6050
@@ -133,106 +125,147 @@ void setup() {
     Serial.println(F(")"));
   }
 
-  //WIFI
-  // Serial.println("Connecting to wifi");
-  // WiFi.mode(WIFI_STA);
-  // WiFi.begin(MYSSID, MYPASS);
-  // while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-  //   Serial.println("Connection Failed! Rebooting...");
-  //   delay(5000);
-  //   ESP.restart();
-  // }
-  // //OTA
-  // ArduinoOTA.setHostname("BalancingBotESP32");
-  // ArduinoOTA
-  //   .onStart([]() {
-  //     motorControl.motorsStop();
-  //     String type;
-  //     if (ArduinoOTA.getCommand() == U_FLASH)
-  //       type = "sketch";
-  //     else // U_SPIFFS
-  //       type = "filesystem";
+  Serial.println("Connecting to wifi");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(MYSSID, MYPASS);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+  Serial.println("Connected");
 
-  //     // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-  //     Serial.println("Start updating " + type);
-  //   })
-  //   .onEnd([]() {
-  //     Serial.println("\nEnd");
-  //   })
-  //   .onProgress([](unsigned int progress, unsigned int total) {
-  //     Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  //   })
-  //   .onError([](ota_error_t error) {
-  //     Serial.printf("Error[%u]: ", error);
-  //     if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-  //     else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-  //     else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-  //     else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-  //     else if (error == OTA_END_ERROR) Serial.println("End Failed");
-  //   });
 
-  // ArduinoOTA.begin();
+  // OTA
+  ArduinoOTA.setHostname("BalancingBotESP32");
+  ArduinoOTA
+    .onStart([]() {
+      motorControl.motorsStop();
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
 
-  // Serial.println("Ready tralal");
-  // Serial.print("IP address: ");
-  // Serial.println(WiFi.localIP());
-  // Serial.println("Waiting for OTA for 10 sec");
-  // unsigned long start = millis();
-  // while(millis() - start <= 10000) {
-  //     Serial.print(".");
-  //     ArduinoOTA.handle();
-  //     delay(500);
-  // }
-  Serial.println("");
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+
+  Serial.println("Ready tralal");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println("Waiting for OTA for 10 sec");
+  unsigned long start = millis();
+  while(millis() - start <= 10000) {
+      Serial.print(".");
+      ArduinoOTA.handle();
+      delay(500);
+  }
+  // Serial.println("");
   // Serial.println("Setup motors");
-  // motorControl.attachMotors(MOTOR_A1, MOTOR_A2, MOTOR_B1, MOTOR_B2);
+  motorControl.attachMotors(MOTOR_A1, MOTOR_A2, MOTOR_B1, MOTOR_B2);
 }
 
 // ================================================================
 // ===                    MAIN PROGRAM LOOP                     ===
 // ================================================================
 
-void loop() {
-  if (!dmpReady) return;
-  // if programming failed, don't try to do anything
-  if (mpuInterrupt) {
-    Serial.println("Interrupt!");
-    mpuInterrupt = false;
-  }
 
+void loop() {
+  ArduinoOTA.handle();
+  // if programming failed, don't try to do anything
+  if (!dmpReady || !mpuInterrupt) return;
+  mpuInterrupt = false;
   // read a packet from FIFO
+  static unsigned long lastOutput = 0;
   if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet 
     mpu.dmpGetQuaternion(&q, fifoBuffer);
     mpu.dmpGetGravity(&gravity, &q);
     mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-    Serial.print("ypr\t");
-    Serial.print(ypr[0] * 180 / M_PI);
-    Serial.print("\t");
-    Serial.print(ypr[1] * 180 / M_PI);
-    Serial.print("\t");
-    Serial.print(ypr[2] * 180 / M_PI);
-
-    // display Euler angles in degrees
     mpu.dmpGetEuler(euler, &q);
+    double angle = abs(ypr[2]);
+    int speed = min(angle, M_PI) * 100 / M_PI;
+    Serial.print("speed: "); Serial.println(speed); 
+    if (ypr[2] < 0) {
+      motorControl.motorReverse(0, speed);
+      motorControl.motorReverse(1, speed);
+    } else {
+      motorControl.motorForward(0, speed);
+      motorControl.motorForward(1, speed);
+    }
+    Serial.print(ypr[2] * 180 / M_PI);
+    unsigned long now = millis();
+    if (now - lastOutput > 250) {
+      lastOutput = now;
+#ifdef OUTPUT_READABLE_QUATERNION
+    // display quaternion values in easy matrix form: w x y z
+    Serial.print("quat\t");
+    Serial.print(q.w);
+    Serial.print("\t");
+    Serial.print(q.x);
+    Serial.print("\t");
+    Serial.print(q.y);
+    Serial.print("\t");
+    Serial.println(q.z);
+#endif
+
+#ifdef OUTPUT_READABLE_EULER
     Serial.print("euler\t");
     Serial.print(euler[0] * 180 / M_PI);
     Serial.print("\t");
     Serial.print(euler[1] * 180 / M_PI);
     Serial.print("\t");
     Serial.println(euler[2] * 180 / M_PI);
-  }
+#endif
 
-  // ArduinoOTA.handle();
-  // Serial.println("loop");
-  // for (int i = 0; i <= 100; i++) {
-  //   motorControl.motorForward(0, i);
-  //   motorControl.motorForward(1, i);
-  //   delay(100);
-  // }
-  // delay(1000);
-  // for (int i = 100; i >= 0; i--) {
-  //   motorControl.motorForward(0, i);
-  //   motorControl.motorForward(1, i);
-  //   delay(100);
-  // }
+
+    
+#ifdef OUTPUT_READABLE_YAWPITCHROLL
+    Serial.print("ypr\t");
+    Serial.print(ypr[0] * 180 / M_PI);
+    Serial.print("\t");
+    Serial.print(ypr[1] * 180 / M_PI);
+    Serial.print("\t");
+    Serial.print(ypr[2] * 180 / M_PI);
+    Serial.print("\traw\t");
+    Serial.print(ypr[2]);
+    /*
+      mpu.dmpGetAccel(&aa, fifoBuffer);
+      Serial.print("\tRaw Accl XYZ\t");
+      Serial.print(aa.x);
+      Serial.print("\t");
+      Serial.print(aa.y);
+      Serial.print("\t");
+      Serial.print(aa.z);
+      mpu.dmpGetGyro(&gy, fifoBuffer);
+      Serial.print("\tRaw Gyro XYZ\t");
+      Serial.print(gy.x);
+      Serial.print("\t");
+      Serial.print(gy.y);
+      Serial.print("\t");
+      Serial.print(gy.z);
+    */
+    Serial.println();
+
+#endif
+
+    }
+  }
 }
+
